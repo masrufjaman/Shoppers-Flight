f5b0916f528b4da9996c7c00ebd5b5aa37eb3abd
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-10-27 00:32:02 +0600

    change name

--- travelers/apps.py
+++ travelers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TravelerConfig(AppConfig):
+class TravelersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'traveler'
+    name = 'travelers'

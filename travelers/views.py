82835d2dfe174d0807fd104604df51883a7eba3e
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-10-27 18:08:32 +0600

    connect travelers post page with a html template

--- travelers/views.py
+++ travelers/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def post_request(request):
-    return HttpResponse("Post Page")
+    return render(request, 'index.html')

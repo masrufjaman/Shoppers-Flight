4fe8e56701ede4c23d7343d3e56b22c135e0390b
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-10-29 01:45:52 +0600

    change render request for post_request function

--- travelers/views.py
+++ travelers/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def post_request(request):
-    return render(request, 'index.html')
+    return render(request, 'flightDetails.html')

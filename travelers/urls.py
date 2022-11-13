fed3d66bf3c08c14da0221e9322e7622c9414971
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-11-13 08:13:09 +0600

    frontend for traveler sign up & in option in Django

--- travelers/urls.py
+++ travelers/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.land_page, name='land-page'),
+    path('sign-up/', views.sign_up, name='sign-up'),
     path('post-request/', views.post_request, name='post-request'),
 ]
--- travelers/views.py
+++ travelers/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def land_page(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
 def post_request(request):
-    return render(request, 'flightDetails.html')
+    return render(request, "flightDetails.html")
+
+
+def sign_up(request):
+    return render(request, "travelerSign.html")

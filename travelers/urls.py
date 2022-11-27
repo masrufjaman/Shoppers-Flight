ec5075df89b80ea6ab99aa353eed18a240baf59c
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-11-27 10:20:25 +0600

    login

--- travelers/urls.py
+++ travelers/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.land_page, name='land-page'),
     path('sign-up/', views.sign_up, name='sign-up'),
+    path('login/', views.login, name='login'),
     path('post-request/', views.post_request, name='post-request'),
 ]
--- travelers/views.py
+++ travelers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Traveler
+from django.contrib.auth.models import User, auth
 
 
 def land_page(request):
@@ -13,19 +14,33 @@ def post_request(request):
 
 def sign_up(request):
     if request.method == "POST":
-        if request.POST.get("register"):
-            fullname = request.POST["fullname"]
-            email = request.POST["email"]
-            phone = request.POST["phone"]
-            password = request.POST["psw"]
+        fullname = request.POST["fullname"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        password = request.POST["psw"]
 
-            user = Traveler.objects.create(
-                fullname=fullname, email=email, phone=phone, password=password
-            )
-            user.save()
+        user = Traveler.objects.create(
+            fullname=fullname, email=email, phone=phone,password=password
+        )
+        user.save()
 
-            return redirect("post-request")
-        else:
-            return redirect("post-request")
+        return redirect("post-request")
     else:
         return render(request, "travelerSign.html")
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["email"]
+        password = request.POST["psw"]
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("post-request")
+        else:
+            # messages.error(
+            #     request, f'Invalid credentials...')
+            return redirect("sign-up")
+    else:
+        return render(request, "login.html")

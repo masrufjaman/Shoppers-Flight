81d17e1810cbf2d0bfb7e8e4e98e870740dbc78f
Abrar Shahriar <94299568+Abrar-Shahrirar@users.noreply.github.com>
2022-12-04 02:49:00 +0600

    again try

--- travelers/models.py
+++ travelers/models.py
@@ -5,13 +5,14 @@ class TravelersPost(models.Model):
     arrival_date = models.DateField()
     destination_country = models.CharField(max_length=50)
     origin_country = models.CharField(max_length=50)
-    # passport_no = models.IntegerField()
+    passport_no = models.IntegerField()
     ticket_no = models.IntegerField()
     airlines_name = models.CharField(max_length=50)
     email = models.EmailField()
     contact_no = models.IntegerField()
     class Meta:
         db_table="flightdetails"
+        
 
 
 class Traveler(models.Model):
